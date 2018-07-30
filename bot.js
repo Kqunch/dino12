8b299ea5b8cfb3199c8373dd0798803b0346e27a
Kqunch <38544203+Kqunch@users.noreply.github.com>
2018-07-30 14:56:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,21 +5,12 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    var msg = message.content.toLowerCase();
-    if (msg === 'd!tea') {
-      message.reply(':tea:');
-    } else if (msg === 'd!blendscharacters') {
-      message.reply('https://goo.gl/images/zdC8DT');
-    } else if (msg === 'd!re:zero') {
-      message.reply('https://www.watchcartoononline.io/anime/re-zero-kara-hajimeru-isekai-seikatsu-english-subbed');
-    } else if (msg === '^help') {
-      message.reply('To use this bot please enter the name of an anime with ^ in front of it and if we have that anime link it will');
-    } else if (msg === '^list') {
-      message.reply('assassination classroom, blend s, re:zero ');
-    } else if (msg === '^trash') {
-      message.reply('https://www.watchcartoononline.io/anime/sword-art-online');
-    }
+ client.on("MessageReactionAdd", function(users) {
+if (message.content === "!verify") {
+  users.addRole(users.guild.roles.find("Verified✔️", setup.verify))
+} else if (!message.content === "role1") {
+  user.removeRole(users.guild.role.find("Verified✔️", setup.default))
+}
 });
 
 client.login(process.env.BOT_TOKEN);
